73f278f57a859febc756d2bb1d18d0e4225a75b1
trmml <joshtrommel@gmail.com>
2019-02-09 21:57:40 -0800

    Scss

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp')
+    , sass = require('gulp-sass');
+
+gulp.task('default', () => {
+    return gulp
+        .src('public/css/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('public/css/*', gulp.series('default'));
+});
