489f927de5e21e5b2e619ba90bc2d69f778a3a57
trmml <joshtrommel@gmail.com>
2019-02-09 21:29:57 -0800

    Go wild

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
-    , app = express()   
-    , PORT = 8080;
+    , app = express()
+    , morgan = require('morgan')
+    , exphbs = require('express-handlebars')
+    , bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    res.send('test');
+    res.render('home');
 });
 
 app.listen(PORT, () => {
     console.log(`Hosting on ${PORT}`);
-})
+});
