d3a6ebe0c96d8c7b53bce6fb32a5af467e9d8921
shinz <sethhinz@me.com>
2019-02-10 07:47:10 -0800

    Took a nap

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+function loveMachine(query, req, cb) {
+    const db = require('./database');
+    const symptoms = Object.keys(db.symptoms);
+    
+    const search = new fuzzy(symptoms, { caseSensitive: false });
+    const mainResult = search.search(query);
+
+    cb(mainResult);
+}
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -26,53 +36,62 @@ app.get('/search', (req, res, next) => {
         // bitch you aint got queries
         next();
     } else {
-        const query = req.query.query;
-        const db = require('./database');
-        const symptoms = Object.keys(db.symptoms);
+        loveMachine(req.query.query, req, (mainResult) => {
+            const db = require('./database');
+            const symptoms = Object.keys(db.symptoms);
 
-        const search = new fuzzy(symptoms, { caseSensitive: false });
-        const mainResult = search.search(query);
+            const search = new fuzzy(symptoms, { caseSensitive: false });                                                                                    
 
-        // sum weight x number of occuruances squared 
+            // sum weight x number of occuruances squared 
 
-        var render = {
-            query: query,
-            mainResult: mainResult,
-            results: []
-        }
-
-        var scoring = {} // This will store conditon names as well as their sum weights and total occurences, like: {"common cold": {weight: 0.5, count: 2}}
-
-        mainResult.forEach(key => { // For every symptom name matched by the fuzzy finder,
-            conditions = db.symptoms[key] // We get the the possible conditions underneath it in the database
-
-            for(var key in conditions) { // Then, we iterate over each condition in the list.
-                if(key in scoring) { // If our scoring object has already come across this condition, we add the weight and increment the occurence counter.
-                    // add weight, counter
-                    scoring[key].weight += conditions[key]
-                    scoring[key].count++
-                } else { // If this is the first time we've come across this condition, we must intialize the object in the score counter.
-                    scoring[key] = {weight: conditions[key], count: 1}
-                }
+            var render = {
+                mainResult: mainResult,
+                results: []
             }
+
+            var scoring = {} // This will store conditon names as well as their sum weights and total occurences, like: {"common cold": {weight: 0.5, count: 2}}
+
+            mainResult.forEach(key => { // For every symptom name matched by the fuzzy finder,
+                conditions = db.symptoms[key] // We get the the possible conditions underneath it in the database
+
+                for(var key in conditions) { // Then, we iterate over each condition in the list.
+                    if(key in scoring) { // If our scoring object has already come across this condition, we add the weight and increment the occurence counter.
+                        // add weight, counter
+                        scoring[key].weight += conditions[key]
+                        scoring[key].count++
+                    } else { // If this is the first time we've come across this condition, we must intialize the object in the score counter.
+                        scoring[key] = {weight: conditions[key], count: 1}
+                    }
+                }
+            })
+
+            // At this point in the code, scoring contains a dictionary with possible condition names, the weights attributed to them, and how many
+            // times they showed up in the search (a measure of how likely the particular condition is)
+
+            // So, we need to calculate the transformed score (which accounts for the number of occurences), and then sort them from most to least likely.
+
+            for(var condition in scoring) {
+                scoring[condition].finalScore = scoring[condition].weight * Math.pow(scoring[condition].count, 2)
+            }
+
+            var sortable = Object.keys(scoring).map(key => {
+                return [key, scoring[key].finalScore]
+            });
+
+            sortable.sort((first, second) => {
+                return second[1] - first[1]
+            })
+
+            sortable.forEach(diagnosis => {
+                render.results.push({title: diagnosis[0], info: 'wow you\'re sick'})
+            })
+
+            console.log(render);
+
+            // sum weight x number of occuruances squared 
+
+            res.render('search', render);
         })
-
-        // At this point in the code, scoring contains a dictionary with possible condition names, the weights attributed to them, and how many
-        // times they showed up in the search (a measure of how likely the particular condition is)
-
-        // So, we need to calculate the transformed score (which accounts for the number of occurences), and then sort them from most to least likely.
-
-        for(var condition in scoring) {
-            scoring[condition].finalScore = scoring[condition].weight * Math.pow(scoring[condition].count, 2)
-        }
-
-        console.log(scoring)
-
-        console.log(render);
-
-        // sum weight x number of occuruances squared 
-
-        res.render('search', render);
     }
 });
 
